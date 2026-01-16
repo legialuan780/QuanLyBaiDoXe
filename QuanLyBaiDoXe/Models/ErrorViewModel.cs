bf2055ad1f0cc5e978de8ef1b56b8fdaffd7ceff
plak101 <phamleanhkhoa2005@gmail.com>
2026-01-16 21:06:36 +0700

    init project

---
+++ QuanLyBaiDoXe/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuanLyBaiDoXe.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
